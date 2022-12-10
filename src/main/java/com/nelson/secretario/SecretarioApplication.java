724be48c48ab767b24cb4587aa2b6c5d48dec06a
nelsonSilva07 <nelson.silva.1984@gmail.com>
2022-12-10 02:05:46 -0300

    commit inicial

---
+++ src/main/java/com/nelson/secretario/SecretarioApplication.java
@@ -0,0 +1,13 @@
+package com.nelson.secretario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretarioApplication.class, args);
+	}
+
+}
